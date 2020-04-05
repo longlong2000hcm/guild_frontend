c0b3043bb5778ae9f355d89f29ed23464e06f31e
EmilieLed <emilieledoussal@yahoo.fr>
2020-04-05 13:41:16 +0300

    Made a custom list for displaying jobs but it still need to be linked to the database. For the moment, the elements inside are written from the Main Activity

--- app/src/main/java/project/guild/MainActivity.java
+++ app/src/main/java/project/guild/MainActivity.java
@@ -3,12 +3,29 @@ package project.guild;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Job> jobList;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        List<Job> jobList = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.listView);
+
+
+        jobList.add(new Job("Babysitting", "description","Oulu"));
+        jobList.add(new Job("Walk the dogs", "description","Helsinki"));
+
+        JobListAdapter adapter = new JobListAdapter(this, R.layout.jobs, jobList);
+
+        listView.setAdapter(adapter);}}
