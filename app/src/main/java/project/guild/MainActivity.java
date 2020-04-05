2be3da6a45d5d24759ebb9e832ad3f747c0ba6ea
Oscar Blide <oscarblide@gmail.com>
2020-04-05 14:53:39 +0200

    Added the button to create a job view

--- app/src/main/java/project/guild/MainActivity.java
+++ app/src/main/java/project/guild/MainActivity.java
@@ -2,13 +2,33 @@ package project.guild;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button BtnAddView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtnAddView = findViewById(R.id.BtnAddJob);
+
+        BtnAddView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                moveToactivity_add_job_view();
+            }
+        });
+
+    }
+
+    private void moveToactivity_add_job_view(){
+        Intent intent = new Intent(MainActivity.this, AddJobView.class);
+        startActivity(intent);
     }
 }
