56d2e1b26e09e25fd2bd2fd0f3f699d6d8104773
Oscar Blide <oscarblide@gmail.com>
2020-04-08 13:04:01 +0200

    Added a disclaimer with checkbox

--- app/src/main/java/project/guild/AddJobView.java
+++ app/src/main/java/project/guild/AddJobView.java
@@ -3,7 +3,13 @@ package project.guild;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.StyleSpan;
+import android.text.style.TypefaceSpan;
+import android.widget.TextView;
 
 public class AddJobView extends AppCompatActivity {
 
@@ -14,5 +20,19 @@ public class AddJobView extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView textView = findViewById(R.id.DisclaimerText);
+
+                String text = "\nDisclaimer\nCreating this ad adds a small one-time fee." +
+                        "This means that the business keeps up and the application is used seriously. By checking in you agree and approve these conditions.\n\n";
+
+        SpannableString ss = new SpannableString(text);
+
+        StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
+
+        ss.setSpan(boldSpan, 0, 11, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        textView.setText(ss);
+
     }
 }
