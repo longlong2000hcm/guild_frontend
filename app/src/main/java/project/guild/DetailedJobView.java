968bddf8558dae3c608cebde181fd3f42f2ed19e
Joel <joel.m.maenpaa@gmail.com>
2020-04-10 13:52:39 +0300

    simple functionality for the detailed job view

--- app/src/main/java/project/guild/DetailedJobView.java
+++ app/src/main/java/project/guild/DetailedJobView.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,14 +18,25 @@ public class DetailedJobView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed_job_view);
 
+        // get data from previous activity
         final Intent intent = getIntent();
         final Job job = (Job) intent.getSerializableExtra("JOB");
 
-        Button btn = findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        // set up variables for each item
+        String title = job.title;
+        String description = job.description;
+        String location = job.location;
+
+        // set title
+        TextView title_text = findViewById(R.id.title);
+        title_text.setText(title);
+
+        // set onclick for back button
+        Button back_button = findViewById(R.id.back);
+        back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("THE FING JOB", "onClick: " + job);
+                finish();
             }
         });
     }
