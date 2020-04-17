9bd3e41496b1d283430bb29649eb9aabe544153d
Oscar Blide <oscarblide@gmail.com>
2020-04-17 10:07:28 +0200

    Clean up code and added Splash screen

---
+++ app/src/main/java/project/guild/SplashScreenActivity.java
@@ -0,0 +1,24 @@
+package project.guild;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+
+
+    }
+}
